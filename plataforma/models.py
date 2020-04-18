ea7756e86b90f499e4aa89c1ef442341027a95e7
migherize <migherize@gmail.com>
2020-04-18 02:10:36 -0400

    Arreglos a base de dato origianl, anexos de diagram ERE y relacional y conexion con PostgresSQL

--- plataforma/models.py
+++ plataforma/models.py
@@ -20,13 +20,19 @@ class Perfil(models.Model):
 	)
 	titulo = models.CharField(max_length=2, choices = title)
 	
-	user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+	username = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+
+	def __unicode__(self):
+		return self.username
 
 class Foto(models.Model):
 	ruta = models.ImageField(
 		upload_to ='../imagenes/Profiles/', height_field=None, width_field=None, max_length=100)
 	
-	perfil =models.OneToOneField(Perfil, null=True, blank=True, on_delete=models.CASCADE)
+	username =models.OneToOneField(Perfil, null=True, blank=True, on_delete=models.CASCADE)
+
+	def __unicode__(self):
+		return self.username
 
 class Direccion(models.Model):
 	calle = models.CharField(max_length=30, null=True, blank=True, default=None)
@@ -34,30 +40,45 @@ class Direccion(models.Model):
 	cuidad = models.CharField(max_length=30, null=True, blank=True, default=None)
 	pais = models.CharField(max_length=30, null=True, blank=True, default=None)
 
-	perfil = models.ForeignKey(Perfil, related_name="addresses", on_delete=models.CASCADE)
+	username = models.ForeignKey(Perfil, related_name="addresses", on_delete=models.CASCADE)
+
+	def __unicode__(self):
+		return self.username
 
 class ELO(models.Model):
-	Type = models.CharField(max_length=10, primary_key=True)
-	Num_win = models.IntegerField()
-	Num_lose = models.IntegerField()
-	Num_draw = models.IntegerField()
+	tipo = models.CharField(max_length=10, primary_key=True)
+	num_win = models.IntegerField()
+	num_lose = models.IntegerField()
+	num_draw = models.IntegerField()
 
-	perfil = models.ForeignKey(Perfil, related_name="rankings", null=True, blank=True, on_delete=models.CASCADE)
+	username = models.ForeignKey(Perfil, related_name="rankings", null=True, blank=True, on_delete=models.CASCADE)
 
+	def __unicode__(self):
+		return self.username
 
 
 #Zona de clase 
 
 class Curso(models.Model):
+	codigo = models.AutoField(primary_key=True)
 	nombre = models.CharField(max_length=30, null=True, blank=True, default=None)
+
+	def __unicode__(self):
+		return self.nombre
 
 class Estudiante(models.Model):
 	promedio = models.IntegerField()
-	user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+	username = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+
+	def __unicode__(self):
+		return self.username
 
 class Tutor(models.Model):
 	Salario = models.IntegerField()
-	User = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+	username = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+
+	def __unicode__(self):
+		return self.username
 
 class Seccion_De(models.Model):
 	fecha_inicio =  models.DateTimeField()
@@ -69,8 +90,12 @@ class Seccion_De(models.Model):
 class Seccion(models.Model):
 	nombre = models.CharField(max_length=30, null=True, blank=True, default=None)
 
-	seccion_de = models.ForeignKey(Seccion_De,related_name ="modulo", null=True, blank=True, on_delete=models.CASCADE)
+	codigo = models.ForeignKey(Seccion_De,related_name ="modulo", null=True, blank=True, on_delete=models.CASCADE)
 	tutor = models.ForeignKey(Tutor, related_name ="profesor", null=True, blank=True, on_delete=models.CASCADE)
+
+	def __unicode__(self):
+		return self.nombre
+
 
 class Asiste_a(models.Model):
 	nota = models.IntegerField()
@@ -84,51 +109,69 @@ class Amigo(models.Model):
 	
 	aceptar = models.BooleanField(default=False)
 
-	followers = models.ForeignKey('auth.User', null=True,related_name="seguidores", blank=True, on_delete=models.CASCADE)
-	follow = models.ForeignKey('auth.User', null=True,related_name="seguidor", blank=True, on_delete=models.CASCADE)
+	seguidores = models.ForeignKey('auth.User', null=True,related_name="seguidores", blank=True, on_delete=models.CASCADE)
+	siguiendo = models.ForeignKey('auth.User', null=True,related_name="seguidor", blank=True, on_delete=models.CASCADE)
 
 	class Meta:
-		unique_together = ['followers','follow']
+		unique_together = ['seguidores','siguiendo']
 
 #Zona de Partida
 
-class Estructura_peones(models.Model):
+class Partida(models.Model):
 	codigo = models.AutoField(primary_key=True)
-	nombre = models.CharField(max_length=30)
-	
-	estilo = models.ForeignKey('Estilo_de_Partida', related_name="estructura", null=True, blank=True, on_delete=models.CASCADE)
+	contrincante = models.CharField(max_length=30)
+	jugadas = models.TextField()
+	fecha_hora =  models.DateTimeField()
+	resultado = models.CharField(max_length=30)
 
+	usuario = models.ForeignKey('auth.User', related_name="id_partida",null=True, blank=True, on_delete=models.CASCADE)
+	tipo = models.OneToOneField('Tipo', on_delete=models.CASCADE)
+	apertura = models.OneToOneField('Apertura', on_delete=models.CASCADE)
+	tipo = models.OneToOneField('Tipo', on_delete=models.CASCADE)
+
+	def __unicode__(self):
+		return self.contrincante
+	
 class Tipo(models.Model):
 	codigo = models.AutoField(primary_key=True)
 	nombre = models.CharField(max_length=30)
+
+	def __unicode__(self):
+		return self.nombre
 
 class Apertura(models.Model):
 	codigo = models.CharField(max_length=5, primary_key=True)
 	nombre = models.CharField(max_length=30)
 
+	def __unicode__(self):
+		return self.nombre
+
 class Medio_Juego(models.Model):
 	codigo = models.AutoField(primary_key=True)
 	nombre = models.CharField(max_length=30)
 
-	estilo = models.ForeignKey('Estilo_de_Partida', related_name="mediojuego", null=True, blank=True, on_delete=models.CASCADE)
+	partida = models.ForeignKey(Partida, related_name="mediojuego", null=True, blank=True, on_delete=models.CASCADE)
+
+	def __unicode__(self):
+		return self.nombre
 
 class Final(models.Model):
 	codigo = models.AutoField(primary_key=True)
 	nombre = models.CharField(max_length=30)
 
-	estilo = models.ForeignKey('Estilo_de_Partida', related_name="final", null=True, blank=True, on_delete=models.CASCADE)
+	partida = models.ForeignKey(Partida, related_name="final", null=True, blank=True, on_delete=models.CASCADE)
 
-class Partida(models.Model):
-	user = models.CharField(max_length=30)
-	contrincante = models.CharField(max_length=30)
-	fecha_hora =  models.DateTimeField()
-	resultado = models.CharField(max_length=30)
+	def __unicode__(self):
+		return self.nombre
 
-	partida = models.ForeignKey('auth.User', related_name="id_partida",null=True, blank=True, on_delete=models.CASCADE)
+class Estructura_Peones(models.Model):
+	codigo = models.AutoField(primary_key=True)
+	nombre = models.CharField(max_length=30)
+	
+	partida = models.ForeignKey(Partida, related_name="estructura", null=True, blank=True, on_delete=models.CASCADE)
 
-class Estilo_de_Partida(models.Model):
-	jugadas = models.TextField()
+	def __unicode__(self):
+		return self.nombre
 
-	partida = models.OneToOneField(Partida, on_delete=models.CASCADE)
-	tipo_estilo = models.OneToOneField(Tipo, on_delete=models.CASCADE)
-	apertura = models.OneToOneField(Apertura, on_delete=models.CASCADE)
+
+	
